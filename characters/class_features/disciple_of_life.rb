0036834f8e0f2e755f8308da53dccb61d915e13e
wylliamjudd <wylliam@toptal.com>
2020-04-24 03:26:33 -0700

    fix message for disciple of life

--- characters/class_features/disciple_of_life.rb
+++ characters/class_features/disciple_of_life.rb
@@ -2,7 +2,9 @@ module DiscipleOfLife
   private
 
   def roll_healing
-    super + 2 + self.class::Level
+    healing = healing_dice.roll + character.spell_ability_score + 2 + self.class::Level
+    p "#{character.name} heals #{target.name} for #{healing}."
+    healing
   end
 
   def average_healing
